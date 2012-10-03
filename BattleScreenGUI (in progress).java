0f139284c4e4a8fa89a873dac16611fb8b607a37
Daniel Brady <dabrady@indiana.edu>
2012-10-03 18:35:03 -0400

    added Battle Screen mock-ups and layout

---
+++ BattleScreenGUI (in progress).java
@@ -0,0 +1,118 @@
+//There should be two of these. One for wild Pokemon battles and one for Trainer battles.
+//Perhaps a TrainerBattle object would contain a Battle object...
+
+
+/* BATTLE SCREEN LAYOUT:
+ * Frame
+ *  |
+ *	-> Menubar / Button array
+ *    |
+ *    -> Pokedex, opens JDialog
+ *    |
+ *    -> Team / Pokemon, opens JDialog or a mere drop-down list of your team and their statuses
+ *    |
+ *    -> Bag, opens JDialog
+ *	  |
+ *    -> Save, does what it needs (opens Save menu or whatever we decide to implement)
+ *	  |
+ *	  -> Run, opens JDialog confirming action or denies your request due to battle status
+ *  |
+ *  -> ContentPanel, grid layout 1x3
+ *    |
+ * 	  -> Zeroth cell
+ * 		|
+ * 		-> Player1 panel, with trainer name and team status
+ *	  |
+ *	  -> First cell
+ *		|
+ *		-> Battle simulator panel, grid layout 2x1
+ *	      |
+ *		  -> Zeroth cell
+ *			|
+ *			-> Battle animation
+ *	      |
+ *		  -> First cell, grid layout 2x1
+ *	      	|
+ *		  	-> Zeroth cell
+ *			  |
+ *			  -> Text ("battle announcer guy")
+ *			|
+ *			-> First cell, grid layout 4x2
+ *			  |
+ *			  -> (0, 0)
+ *				|
+ *				-> Move 1
+ *			  |
+ *			  -> (0, 1)
+ *				|
+ *				-> Move 2
+ *			  |
+ *			  -> (1, 0)
+ *				|
+ *				-> PP for move 1
+ *			  |
+ *			  -> (1, 1)
+ *				|
+ *				-> PP for move 2
+ *			  |
+ *			  -> (2, 0)
+ *				|
+ *				-> Move 3
+ *			  |
+ *			  -> (2, 1)
+ *				|
+ *				-> Move 4
+ *			  |
+ *			  -> (3, 0)
+ *				|
+ *				-> PP for move 3
+ *			  |
+ *			  -> (3, 1)
+ *				|
+ *				-> PP for move 4
+ *	  |
+ *	  -> Second cell
+ *		|
+ *		-> Player 2 panel, with trainer name and team status
+ */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
